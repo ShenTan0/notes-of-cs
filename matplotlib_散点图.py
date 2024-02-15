37db60fff2b121290855a97a9050a4926cf4118b
ShenTan0 <143203810+ShenTan0@users.noreply.github.com>
2024-02-15 15:25:39 +0800

    Update matplotlib_散点图.py

--- matplotlib_散点图.py
+++ matplotlib_散点图.py
@@ -4,8 +4,8 @@ import numpy as np
 x=[1,1,4,5,1,4]
 y=[1,9,1,9,8,1]
 c=['red','red','blue','blue','pink','pink']
-plt.scatter(x,y,color=c) #或者color=''
-
+plt.scatter(x,y,color=c,s=range(1,7),alpha=0.5) #或者color=''
+#s设定尺寸，alpha设定透明度
 
 
 plt.show()
