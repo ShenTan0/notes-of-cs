570abede3c033345bfbe19f7d843cda243577106
ShenTan0 <143203810+ShenTan0@users.noreply.github.com>
2024-02-15 15:09:41 +0800

    Create matplotlib_散点图.py

---
+++ matplotlib_散点图.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=[1,1,4,5,1,4]
+y=[1,9,1,9,8,1]
+c=['red','red','blue','blue','pink','pink']
+plt.scatter(x,y,color=c) #或者color=''
+
+
+
+plt.show()
